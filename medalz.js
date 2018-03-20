5be98082fd466d04669697ffa0fe1062fbc757d6
Valery Chen <cpan3@stuy.edu>
2018-03-19 20:59:47 -0400

    numbers added

--- medalz.js
+++ medalz.js
@@ -6,21 +6,24 @@ var SK_medal_count = [5, 8, 4];
 var data = US_medal_count;
 
 var set = function(){
-  var circles = d3.selectAll("circle");
-  circles.data(data);
-  circles.attr("r", function(d){return 11 * d});
+    var circles = d3.selectAll("circle");
+    circles.data(data);
+    circles.attr("r", function(d){return 11 * d});
+    var texts = d3.selectAll("text");
+    texts.data(data);
+    texts.html(function(d){return d});
 }
 
 var switch_button = function(e){
-  if (data == US_medal_count){
-    title.innerHTML = "South Korea";
-    data = SK_medal_count;
-  }
-  else if (data == SK_medal_count){
-    title.innerHTML = "US";
-    data = US_medal_count;
-  }
-  set();
+    if (data == US_medal_count){
+	title.innerHTML = "South Korea";
+	data = SK_medal_count;
+    }
+    else if (data == SK_medal_count){
+	title.innerHTML = "US";
+	data = US_medal_count;
+    }
+    set();
 }
 
 switch_btn.addEventListener("click", switch_button);
